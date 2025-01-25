5805e656c8224942a770e4f7b3677b4f9d4d1585
dangoodie <dan.gooden.dev@gmail.com>
2025-01-26 02:25:31 +1100

    Starting Maze class

---
+++ maze.py
@@ -0,0 +1,23 @@
+from cell import Cell
+from graphics import Window
+
+
+class Maze:
+    def __init__(
+            self,
+            x1: int,
+            y1: int,
+            num_rows: int,
+            num_cols: int,
+            cell_size_x: int,
+            cell_size_y: int,
+            win: Window
+        ):
+        self._x1 = x1
+        self._y1 = y1
+        self._num_row = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
+        self._cells = None
