bda8601c23ed5b8e472621ede3d5f8bc8bf0be44
dangoodie <dan.gooden.dev@gmail.com>
2025-01-25 21:02:47 +1100

    drawing lines on the screen

--- graphics.py
+++ graphics.py
@@ -1,5 +1,18 @@
 from tkinter import Tk, BOTH, Canvas
 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point1: Point, point2: Point):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas: Canvas, fill_color):
+        canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2)
+
 class Window:
     def __init__(self, width, height):
         self.__root = Tk()
@@ -21,3 +34,6 @@ class Window:
 
     def close(self):
         self.__running = False
+
+    def draw_line(self, line: Line, fill_color):
+        line.draw(self.__canvas, fill_color)
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from graphics import Window
+from graphics import *
 
 def main():
     win  = Window(800, 600)
+
+    win.draw_line(Line(Point(100,100), Point(400,400)), "red")
+    win.draw_line(Line(Point(400,100), Point(100,400)), "red")
+
     win.wait_for_close()
 
 if __name__ == "__main__":
