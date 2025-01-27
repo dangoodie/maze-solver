222df300e0cc3ec848835ff9a9869625ffd37197
dangoodie <dan.gooden.dev@gmail.com>
2025-01-27 13:49:58 +1100

    Maze class now working.

--- maze.py
+++ maze.py
@@ -1,5 +1,8 @@
+import time
+
 from cell import Cell
 from graphics import Window
+
 
 
 class Maze:
@@ -15,9 +18,35 @@ class Maze:
         ):
         self._x1 = x1
         self._y1 = y1
-        self._num_row = num_rows
+        self._num_rows = num_rows
         self._num_cols = num_cols
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._win = win
-        self._cells = None
+        self._cells = []
+
+        self._create_cells()
+
+    def _create_cells(self):
+        for i in range(self._num_cols):
+            col = []
+            for j in range(self._num_rows):
+                col.append(Cell(self._win))
+            self._cells.append(col)
+        
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._draw_cell(i, j)
+
+    def _draw_cell(self, i, j):
+        x1 = self._x1 + (j * self._cell_size_x)
+        y1 = self._y1 + (i * self._cell_size_y)
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
+        
+        self._cells[i][j].draw(x1, y1, x2, y2)
+        self._animate()
+    
+    def _animate(self):
+        self._win.redraw()
+        time.sleep(0.05)
